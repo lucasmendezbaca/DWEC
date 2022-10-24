2fecfd9ccbc5622ba8a35b2d5c6666cfe96826b0
Lucas Méndez <lucasmendezbaca@gmail.com>
2022-10-24 23:30:32 +0200

    eventos

--- Tema 4/Relacion 1/4.js
+++ Tema 4/Relacion 1/4.js
@@ -1,18 +1,23 @@
-document.addEventListener('click', main);
+const body = document.getElementById("body");
+body.onload = main;
 
 function main() {
     canvas();
 }
 
 function canvas() {
-    document.write("<table>");
+    document.write("<table cellspacing='0' cellpadding='0'>");
     for(let j=0; j<=100; j++) {
         document.write('<tr>');
         for(let i=0; i<=100; i++) {
-            document.write('<td width=5px; height=5px>');
+            document.write('<td onmouseover="pintarCeldas(this)" width="5px" height="5px>"');
             document.write('</td>');
         }
         document.write('<tr>');
     }
     document.write("</table>");
+}
+
+function pintarCeldas(e) {
+    e.style.backgroundColor = "red";
 }
