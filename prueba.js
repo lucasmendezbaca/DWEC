d1d256d65a1fafae4581b56d4f76810f83a49972
Lucas Méndez <lucasmendezbaca@gmail.com>
2022-11-06 23:02:36 +0100

    reloj

---
+++ prueba.js
@@ -0,0 +1,23 @@
+// funcion que muestre un contador hacia atras de 60 segundos
+// function contador() {
+//     var segundos = 60;
+//     var intervalo = setInterval(function () {
+//         segundos--;
+//         if (segundos == 0) {
+//             clearInterval(intervalo);
+//         }
+//         console.log(segundos);
+//     }, 1000);
+// }
+
+// contador();
+
+// function que simule un reloj digital que actualice cada segundo
+// function reloj() {
+//     var fecha = new Date();
+//     var hora = fecha.getHours();
+//     var minutos = fecha.getMinutes();
+//     var segundos = fecha.getSeconds();
+//     console.log(hora + ":" + minutos + ":" + segundos);
+// }
+// setInterval(reloj, 1000);
