beff4be888a806e1308dd97ac3d794efc9a6126f
Lucas Mendez <lucasmendezbaca@gmail.com>
2022-11-10 10:17:22 +0100

    handman

--- Tema 5/Relacion 3/Ejercicio 2/index.js
+++ Tema 5/Relacion 3/Ejercicio 2/index.js
@@ -1,1 +1,50 @@
+const palabras = ["HOLA", "ADIOS", "PERRO", "GATO", "CASA", "COCHE", "MESA", "SILLA", "ORDENADOR", "LIBRO", "PAPEL", "LAPIZ", "BOLIGRAFO", "PIZARRA", "VENTANA", "PUERTA", "SUELO", "TECHO", "PARED", "CAMA", "SILLON", "SOFA", "ALFOMBRA", "LAMPARA", "MESA", "SILLA", "ORDENADOR", "LIBRO", "PAPEL", "LAPIZ", "BOLIGRAFO", "PIZARRA", "VENTANA", "PUERTA", "SUELO", "TECHO", "PARED", "CAMA", "SILLON", "SOFA", "ALFOMBRA", "LAMPARA", "MESA", "SILLA", "ORDENADOR", "LIBRO", "PAPEL", "LAPIZ", "BOLIGRAFO", "PIZARRA", "VENTANA", "PUERTA", "SUELO", "TECHO", "PARED", "CAMA", "SILLON", "SOFA", "ALFOMBRA", "LAMPARA", "MESA", "SILLA", "ORDENADOR", "LIBRO", "PAPEL", "LAPIZ", "BOLIGRAFO", "PIZARRA", "VENTANA", "PUERTA", "SUELO", "TECHO", "PARED", "CAMA", "SILLON", "SOFA", "ALFOMBRA", "LAMPARA", "MESA", "SILLA", "ORDENADOR", "LIBRO", "PAPEL", "LAPIZ", "BOLIGRAFO", "PIZARRA", "VENTANA", "PUERTA", "SUELO", "TECHO", "PARED", "CAMA", "SILLON", "SOFA", "ALFOMBRA", "LAMPARA", "MESA", "SILLA", "ORDENADOR", "LIBRO"];
+
+window.onload = function () {
+    const palabra = palabras[Math.floor(Math.random() * palabras.length)];
+    mostrarPosicionesPalabra(palabra);
+}
+
+function mostrarPosicionesPalabra(palabra) {
+    for (let i = 0; i < palabra.length; i++) {
+        const span = document.createElement("span");
+        span.className = "letra_palabra";
+        span.innerText = palabra[i];
+        document.getElementById("palabra").appendChild(span);
+    }
+}
+
+const botones = document.querySelectorAll("button");
+botones.forEach(boton => {
+    boton.addEventListener("click", desabilitarBoton);
+    boton.addEventListener("click", contarIntento);
+    boton.addEventListener("click", mostrarEnPalabra);
+});
+
+var intentos = 10;
+function contarIntento() {
+    intentos--;
+    document.getElementById("intentos").innerText = intentos;
+    if (intentos === 0) {
+        // alert("Has perdido");
+    }
+}
+
+function desabilitarBoton(e) {
+    e.target.disabled = true;
+    e.target.style.opacity = "0.5";
+}
+
+function mostrarEnPalabra(e) {
+    const letra = e.target.innerText;
+    const spans = document.querySelectorAll("span");
+    spans.forEach(span => {
+        if (span.innerText === letra) {
+            span.style.color = "white";
+        }
+    });
+}
+
+
+
 
